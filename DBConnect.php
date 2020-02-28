f626ed649d6e84206148af560341e9fd100e5c38
Rabin Phaiju <rabinphaiju15@gmail.com>
2020-02-28 19:04:02 +0545

    zip file deleted

--- DBConnect.php
+++ DBConnect.php
@@ -1,7 +1,7 @@
 <?php
 
 // Create connection
-$conn = mysqli_connect("localhost", "root","","id11005287_nepalraktasanchar");
+$conn = mysqli_connect("localhost", "root","root","id11005287_nepalraktasanchar");
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
